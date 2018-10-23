012403949dd991e404c084c294f9fca753bdb5d0
rabbitwhile <vallsarnau19@gmail.com>
2018-10-23 12:10:12 +0000

    imatge

--- imatge_update.sh
+++ imatge_update.sh
@@ -8,4 +8,4 @@ mkdir imatge2
 tar -xzvf openwrt-18.06.1-x86-generic-generic-rootfs2.tar.gz -C imatge2/
 mv openwrt-18.06.1-x86-generic-generic-rootfs2.tar.gz openwrt-18.06.1-x86-generic-generic-rootfs.tar.gz
 
-docker build -t rabbyte/docker_pti:latest .
+#docker build -t rabbyte/docker_pti:latest .
