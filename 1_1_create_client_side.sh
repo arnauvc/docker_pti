865e2be547d8a2463648dae8f56108428e4270ae
rabbitwhile <vallsarnau19@gmail.com>
2018-10-30 12:22:18 +0000

    Per dockerhub

--- 1_1_create_client_side.sh
+++ 1_1_create_client_side.sh
@@ -2,7 +2,7 @@
 #Script de creacio dels contenidors per poder simular
 
 	docker create -t  --privileged --name node_1 --network client_network --ip 170.30.2.2 -p 8081:80  alpine:latest 
-	docker create -t  --privileged --name node_2 --network client_network --ip 170.30.2.3 -p 8082:80  rabbyte/docker_pti:testing
+	docker create -t  --privileged --name node_2 --network client_network --ip 170.30.2.3 -p 8082:80  rabbyte/docker_pti:latest
 
 	exit 0
 
